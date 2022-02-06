d76e3dc0aed48b0c9275eb916eb7aca5df8ece88
Milan-Zlamal <82092210+Milan-Zlamal@users.noreply.github.com>
2022-02-06 21:41:24 +0100

    new enums

--- project_values_setup.py
+++ project_values_setup.py
@@ -1,92 +1,120 @@
 from enum import Enum, auto
+from random import *
 
 
+#TODO: lepsi nez enumy vypadaji listy s random choice
 class Gender(Enum):
-    male   = auto()
+    male = auto()
     female = auto()
 
+
 class Tribe_name_origin(Enum):
-    cz = auto()
-    pl = auto()
-    ne = auto()
-    es = auto()
-    fr = auto()
-    gk = auto()
-    it = auto()
-    us = auto()
+    pl = 1
+    ne = 2
+    es = 3
+    fr = 4
+    gk = 5
+    it = 6
+    us = 7
+    cz = 8
 
 class Tribe_surname_origin(Enum):
-    cz = auto()
-    pl = auto()
-    ne = auto()
-    es = auto()
-    fr = auto()
-    gk = auto()
-    it = auto()
-    us = auto()
+    pl = 1
+    ne = 2
+    es = 3
+    fr = 4
+    gk = 5
+    it = 6
+    us = 7
+    cz = 8
 
 #nutno dovymyslet dalsi, aby jsme s nimi i mohli pocitat jinde
 class Physical_characteristic(Enum):
-	sexy = auto()
-	handsome = auto()
-	beautiful = auto()
-	tall = auto()
-	short = auto()
+    "sexy"
+    "handsome"
+    "beautiful"
+    "tall"
+    "short"
 
 class Social_class(Enum):
-    runner = auto()
-    hero = auto()
-    upper = auto()
-    middle = auto()
-    working = auto()
-    total_junkie = auto()       #totalni fetak
-    homeless = auto()           #klasickej homeless chlast
+    "runner"
+    "hero"
+    "upper"
+    "middle"
+    "working"
+    "total_junkie"      #totalni fetak
+    "homeless"          #klasickej homeless chlast
 
 class Positive_characteristic(Enum):
-    creative = auto()
-    brave = auto()
-    smart = auto()
-    exciting = auto()
-    energetic = auto()
-    entertaining = auto()
-    inspiring = auto()
-    gentle = auto()
-    friendly = auto()
-    generous = auto()
-    considerate = auto()
-    bright = auto()
-    kind = auto()
-    giving = auto()
-    loveable = auto()
-    stable = auto()
+    "creative"
+    "brave"
+    "smart"
+    "exciting"
+    "energetic"
+    "entertaining"
+    "inspiring"
+    "gentle"
+    "friendly"
+    "generous"
+    "considerate"
+    "bright"
+    "kind"
+    "giving"
+    "loveable"
+    "stable"
 
 class Negative_characteristic(Enum):
-    disloyal = auto()
-    sneaky = auto()
-    untrustworthy = auto()
-    cowardly = auto()
-    unintelligent = auto()
-    dull = auto()
-    boring = auto()
-    lazy = auto()
-    rude = auto()
-    unkind = auto()
-    standoffish = auto()
-    unfriendly = auto()
-    stingy = auto()
-    greedy = auto()
-    selfish = auto()
-    violent = auto()
-    unstable = auto()
-    sadistic = auto()
-    evil = auto()
+    "disloyal"
+    "sneaky"
+    "untrustworthy"
+    "cowardly"
+    "unintelligent"
+    "dull"
+    "boring"
+    "lazy"
+    "rude"
+    "unkind"
+    "standoffish"
+    "unfriendly"
+    "stingy"
+    "greedy"
+    "selfish"
+    "violent"
+    "unstable"
+    "sadistic"
+    "evil"
 
 # na zaklade tohoto je mozne resit ze jej nekdo sikanoval ci si na nej nedovolili
-class Height():
-	very_tall = auto()
-	tall = auto()
-	average = auto()
-	short = auto()
-	very_short = auto()
+class Height(Enum):
+	"very tall"
+	"tall"
+	"average"
+	"short"
+	"very short"
 
-#TODO: weight atd.
+class Weight(Enum):
+	"very overweight"
+	"overweight"
+	"average"
+	"underweight"
+	"very underweight"
+
+class Build(Enum):
+	"very well-built"
+	"well-built"
+	"average build"
+	"fine build"
+	"very fine build"
+
+class Hair(Enum):
+    "blonde" 
+    "red"
+    "brown"
+    "black"
+    "grey"
+
+
+
+class Riding_enums():
+    gender = Enum("male", "female")
+    tribe_name_origin = random.choice(Tribe_name_origin)

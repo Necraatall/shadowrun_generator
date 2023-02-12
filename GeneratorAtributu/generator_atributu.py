58bf949c5b0e682bf07b3eedecdd020422da1003
Necraatall <seyuki.yamamoro@gmail.com>
2023-02-12 15:43:32 +0100

    save and minor updates

--- GeneratorAtributu/generator_atributu.py
+++ GeneratorAtributu/generator_atributu.py
@@ -15,7 +15,6 @@
 
 """
 import random
-from dataclasses import dataclass
 from typing import Dict
 from collections import OrderedDict
 
@@ -29,92 +28,91 @@ final_atributes: dict= {}
 chosed_tuple: tuple = ()
 # adventages celkovy pocet bodu na obdareni a postizeni
 adventages: int = 0
-# pointspool_skills pocet bodu na skilly, Edge, rasu, Merits and Flows (neg: critters)
-pointspool_skills: int  # na platbu za edge, rasu a skilly
+
 
 HUMAN_RACE_DICT = {
-    'BS': 0,
-    'Metatype': "Human",
-    'Body':         (1, 6, 9, 0),
-    'Agility':      (1, 6, 9, 0),
-    'Reaction':     (1, 6, 9, 0),
-    'Strength':     (1, 6, 9, 0),
-    'Charisma':     (1, 6, 9, 0),
-    'Intuition':    (1, 6, 9, 0),
-    'Logic':        (1, 6, 9, 0),
-    'Willpower':    (1, 6, 9, 0),
-    'Initiative':   (2, 12, 18),
-    'Edge':         (2, 7, 10, 1),
-    'Initiative_Phases': 1,
-    'Metatype Ability': "+1 Edge"
+    'BS':                   0,
+    'Metatype':             "Human",
+    'Body':                 (1, 6, 9, 0),
+    'Agility':              (1, 6, 9, 0),
+    'Reaction':             (1, 6, 9, 0),
+    'Strength':             (1, 6, 9, 0),
+    'Charisma':             (1, 6, 9, 0),
+    'Intuition':            (1, 6, 9, 0),
+    'Logic':                (1, 6, 9, 0),
+    'Willpower':            (1, 6, 9, 0),
+    'Initiative':           (2, 12, 18),
+    'Edge':                 (2, 7, 10, 1),
+    'Initiative_Phases':    1,
+    'Metatype Ability':     "+1 Edge"
 }
 
 ORC_RACE_DICT = {
-    'BS': 20,
-    'Metatype': "Orc",
-    'Body':         (4, 9, 13, 3),
-    'Agility':      (1, 6, 9, 0),
-    'Reaction':     (1, 6, 9, 0),
-    'Strength':     (3, 8, 12, 2),
-    'Charisma':     (1, 5, 7, -1),
-    'Intuition':    (1, 6, 9, 0),
-    'Logic':        (1, 5, 7, -1),
-    'Willpower':    (1, 6, 9, 0),
-    'Initiative':   (2, 12, 18),
-    'Edge':         (1, 6, 9, 0),
-    'Initiative_Phases': 1,
-    'Metatype Ability': "Low-Light Vision"
+    'BS':                   20,
+    'Metatype':             "Orc",
+    'Body':                 (4, 9, 13, 3),
+    'Agility':              (1, 6, 9, 0),
+    'Reaction':             (1, 6, 9, 0),
+    'Strength':             (3, 8, 12, 2),
+    'Charisma':             (1, 5, 7, -1),
+    'Intuition':            (1, 6, 9, 0),
+    'Logic':                (1, 5, 7, -1),
+    'Willpower':            (1, 6, 9, 0),
+    'Initiative':           (2, 12, 18),
+    'Edge':                 (1, 6, 9, 0),
+    'Initiative_Phases':    1,
+    'Metatype Ability':     "Low-Light Vision"
 }
 
 DWARF_RACE_DICT = {
-    'BS': 25,
-    'Metatype': "Dwarf",
-    'Body':         (2, 7, 10, 1),
-    'Agility':      (1, 6, 9, 0),
-    'Reaction':     (1, 5, 7, -1),
-    'Strength':     (3, 8, 12, 2),
-    'Charisma':     (1, 6, 9, 0),
-    'Intuition':    (1, 6, 9, 0),
-    'Logic':        (1, 6, 9, 0),
-    'Willpower':    (2, 7, 10, 0),
-    'Initiative':   (2, 11, 16),
-    'Edge':         (1, 6, 9, 0),
-    'Initiative_Phases': 1,
-    'Metatype Ability': "Thermographic Vision, +2 dice for Body Tests to resist pathogens and toxins"
+    'BS':                   25,
+    'Metatype':             "Dwarf",
+    'Body':                 (2, 7, 10, 1),
+    'Agility':              (1, 6, 9, 0),
+    'Reaction':             (1, 5, 7, -1),
+    'Strength':             (3, 8, 12, 2),
+    'Charisma':             (1, 6, 9, 0),
+    'Intuition':            (1, 6, 9, 0),
+    'Logic':                (1, 6, 9, 0),
+    'Willpower':            (2, 7, 10, 0),
+    'Initiative':           (2, 11, 16),
+    'Edge':                 (1, 6, 9, 0),
+    'Initiative_Phases':    1,
+    'Metatype Ability':     "Thermographic Vision, +2 dice for Body Tests to resist pathogens and toxins"
 }
 
 ELF_RACE_DICT = {
-    'BS': 30,
-    'Metatype': "Elf",
-    'Body':         (1, 6, 9, 0),
-    'Agility':      (2, 7, 10, 1),
-    'Reaction':     (1, 6, 9, 0),
-    'Strength':     (1, 6, 9, 0),
-    'Charisma':     (3, 8, 12, 2),
-    'Intuition':    (1, 6, 9, 0),
-    'Logic':        (1, 6, 9, 0),
-    'Willpower':    (1, 6, 9, 0),
-    'Initiative':   (2, 12, 18),
-    'Edge':         (1, 6, 9, 0),
-    'Initiative_Phases': 1,
-    'Metatype Ability': "Low-Light Vision"
+    'BS':                   30,
+    'Metatype':             "Elf",
+    'Body':                 (1, 6, 9, 0),
+    'Agility':              (2, 7, 10, 1),
+    'Reaction':             (1, 6, 9, 0),
+    'Strength':             (1, 6, 9, 0),
+    'Charisma':             (3, 8, 12, 2),
+    'Intuition':            (1, 6, 9, 0),
+    'Logic':                (1, 6, 9, 0),
+    'Willpower':            (1, 6, 9, 0),
+    'Initiative':           (2, 12, 18),
+    'Edge':                 (1, 6, 9, 0),
+    'Initiative_Phases':    1,
+    'Metatype Ability':     "Low-Light Vision"
 }
 
 TROLL_RACE_DICT = {
-    'BS': 40,
-    'Metatype': "Troll",
-    'Body':         (5, 10, 15, 4),     # 5
-    'Agility':      (1, 5, 7, -1),      # 5
-    'Reaction':     (1, 6, 9, 0),       # 5
-    'Strength':     (5, 10, 15, 4),     # 5
-    'Charisma':     (1, 4, 6, -2),      # 5
-    'Intuition':    (1, 5, 7, -1),      # 5
-    'Logic':        (1, 5, 7, -1),      # 5
-    'Willpower':    (1, 6, 9, 0),       # 4
-    'Initiative':   (2, 11, 16),
-    'Edge':         (1, 6, 9, 1),
-    'Initiative_Phases': 1,
-    'Metatype Ability': "Thermographic Vision, +1 Reach, +1 natural armor (cumulative with worn armor)"
+    'BS':                   40,
+    'Metatype':             "Troll",
+    'Body':                 (5, 10, 15, 4),
+    'Agility':              (1, 5, 7, -1),
+    'Reaction':             (1, 6, 9, 0),
+    'Strength':             (5, 10, 15, 4),
+    'Charisma':             (1, 4, 6, -2),
+    'Intuition':            (1, 5, 7, -1),
+    'Logic':                (1, 5, 7, -1),
+    'Willpower':            (1, 6, 9, 0),
+    'Initiative':           (2, 11, 16),
+    'Edge':                 (1, 6, 9, 1),
+    'Initiative_Phases':    1,
+    'Metatype Ability':     "Thermographic Vision, +1 Reach, +1 natural armor (cumulative with worn armor)"
 }
 
 # this one is prepared for the future
@@ -127,7 +125,8 @@ class Atributes():
         # TODO: definovat nekde nahore ci uplne bokem body pres promennou
         # pointspool celkovy pocet bodu na postavu
         pointspool: int
-
+        # pointspool_skills pocet bodu na skilly, Edge, rasu, Merits and Flows (neg: critters)
+        pointspool_skills: int  # na platbu za edge, rasu a skilly
         # pointspool_atr_start kolik je do zacatku max na atributy - zde rozdeluji vsechny body ktere lze zakoupit (muze
         pointspool_atr_start: int  # na vypocet atributu vcetne bonusu rasy
         # pointspool/2 must be [200, 300, 340, 390, 400, 430, 440, 500, 600, 640, 690]
@@ -148,7 +147,7 @@ class Atributes():
                     (3, 3, 3, 2, 2, 2, 2, 1),
                     (3, 3, 2, 2, 2, 2, 2, 2),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 230:  # 150BP
                 atribute_pool = (
@@ -170,7 +169,7 @@ class Atributes():
                     (4, 3, 3, 3, 3, 3, 2, 2),
                     (3, 3, 3, 3, 3, 3, 3, 2),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
             case 250:  # 170BP
                 atribute_pool = (
                     (5, 5, 3, 3, 3, 2, 2, 2),
@@ -185,7 +184,7 @@ class Atributes():
                     (4, 4, 3, 3, 3, 3, 3, 2),
                     (4, 3, 3, 3, 3, 3, 3, 3),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
             case 275:  # 195 BP
                 atribute_pool = (
                     (6, 5, 4, 3, 3, 3, 1, 1),
@@ -198,7 +197,7 @@ class Atributes():
                     (6, 4, 3, 3, 3, 3, 3, 1),
                     (6, 3, 3, 3, 3, 3, 3, 2),
                     )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
             case 280:  # 200 BP
                 atribute_pool = (
                     (5, 5, 4, 4, 4, 3, 2, 1),
@@ -211,7 +210,7 @@ class Atributes():
                     (4, 4, 4, 4, 4, 4, 2, 2),
                     (4, 4, 4, 4, 4, 3, 3, 2),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 295:  # 215 BP
                 atribute_pool = (
@@ -225,7 +224,7 @@ class Atributes():
                     (6, 5, 5, 3, 3, 3, 3, 1),
                     (6, 5, 5, 3, 3, 3, 2, 2),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 300:  # 220 BP
                 atribute_pool = (
@@ -240,7 +239,7 @@ class Atributes():
                     (4, 4, 4, 4, 4, 4, 2, 2),
                     (4, 4, 4, 4, 4, 3, 3, 2),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 330:  # 250 BP
                 atribute_pool = (
@@ -259,7 +258,7 @@ class Atributes():
                     (5, 5, 4, 4, 4, 4, 4, 3),
                     (5, 4, 4, 4, 4, 4, 4, 4)
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 380: # 300 BP   
                 atribute_pool = (
@@ -272,7 +271,7 @@ class Atributes():
                     (6, 5, 5, 4, 4, 4, 4, 4),
                     (5, 5, 5, 5, 5, 5, 4, 4)
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 400: # 320 BP
                 atribute_pool = (
@@ -281,7 +280,7 @@ class Atributes():
                     (6, 6, 5, 4, 4, 4, 4, 4),
                     (6, 6, 5, 4, 4, 4, 4, 4),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
             case 425: # 345 BP
                 atribute_pool = (
@@ -289,15 +288,15 @@ class Atributes():
                     (6, 6, 6, 5, 5, 4, 3, 3),
                     (6, 6, 6, 4, 4, 4, 4, 4),
                 )
-                chosed_tuple = atribute_pool[random.randint(1, len(atribute_pool))]
+                chosed_tuple = atribute_pool[random.randint(0, len(atribute_pool)-1)]
 
         chosen_tuple = chosed_tuple
 
-        atributes=make_decision(chosen_tuple, pointspool)
+        atributes=make_decision(chosen_tuple, pointspool, pointspool_skills)
         pointspool_skills = atributes[1]
-        return atributes
+        return atributes[0]
 
-def make_decision(chosed_tuple, pointspool) -> dict:
+def make_decision(chosed_tuple:dict, pointspool:int, pointspool_skills:int) -> dict:
     # choose_destiny - urci zda je mag, technomancer, remeslnik, vagus... 
     choose_destiny: int = random.randint(1, 100)
     metahuman_race_decision = 100 # random.randint(96, 100)
@@ -366,6 +365,6 @@ def make_decision(chosed_tuple, pointspool) -> dict:
     final_atributes['Initiative'] = round((celk_atributy['Reaction'][0] + celk_atributy['Intuition'][0])/2)
     final_atributes['Initiative_Phases'] = RACE_DICT['Initiative_Phases']
 
-    # TODO: for the future whe i will make skills
+    # TODO: for the future when i will make skills
     final_atributes['BS']=RACE_DICT['BS']
     return final_atributes, pointspool_skills
